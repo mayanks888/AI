4a645717d2b9459b624a12d0f5af9854cf1ff49e
mayanks888 <mayanksati07@gmail.com>
2018-10-01 18:43:41 +0530

    ofice

---
+++ Python/python_code/playing_with_groupby.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import numpy as np
+data=pd.read_csv("/home/mayank-s/PycharmProjects/Datasets/Berkely_DeepDrive/berkerly_new_useful/Berkerly_no_filter_include_all_classes.csv")
+
+df=data[(data['xmin']!=data['xmax']) & (data['ymin']!=data['ymax'])]
+
+print(df.groupby('class').count())
+
+classes=['person', 'traffic light', 'bus', 'car', 'motor','bike',  'traffic sign']
+# df=data.loc[data['class'].isin(["traffic sign","traffic light"])]
+df=df.loc[data['class'].isin(classes)]
+
+print(df.groupby('class').count())
+
+
+df=df.replace("motor", "cool")
+df=df.replace("bike", "cool")
+df=df.replace("cool", "motorbike")
+df=df.replace("traffic light", "traffic_light")
+df=df.replace("traffic sign", "traffic_sign")
+print(df.groupby('class').count())
+df.to_csv("berkely_ready_to_train.csv")
+print(1)
+
+
+
+
+
+# data=pd.read_csv('/home/mayank-s/Desktop/Link to Datasets/aptiveBB/reddy.csv')
+'''print(data.head())
+print(data.groupby('class').count())
+# this is done to remove if xmin==xmax and ymin==yamax(which is actuallly wrong)
+# df=data[(data['xmin']!=data['xmax']) & (data['ymin']!=data['ymax'])]
+# print(df.head())
+
+grouped=data.groupby(['filename', 'class'])
+a=(grouped.size())
+filename=[]
+car=[]
+other=[]
+for num,dat in enumerate(grouped.size()):
+    print(dat)
+    print(a.index[num][0])
+    print(a.index[num][1])
+    if a.index[num][1]=="car":
+        filename.append(a.index[num][0])
+        car.append(dat)
+
+# df=pd.DataFrame()
+df=pd.DataFrame(filename,car)#, columns=['filename','car'])
+# df=df[filename,car]
+# df.to_csv("Carcount.csv")'''
+
+
+
+'''#now next job is to remove the selected index from this datasets based on car count on carcaount datasets
+cardatsets=pd.read_csv('/home/mayank-s/PycharmProjects/Data_Science_New/Python/python_code/maxcarcaount_3.csv')
+print(cardatsets.head())
+filename=cardatsets.iloc[:,1].values
+
+ran_file=np.random.choice(filename, size=2000, replace=False)
+print(1)
+
+flag=0
+loop = 0
+new=data.groupby('class')
+print(data.groupby('class').count())
+while(1):
+
+    # print(ran_file[loop])
+    # print(data.groupby('class').count())
+    # data.drop(data[ran_file[0]], inplace=True)
+    # data = data.drop(ran_file[loop], axis=0)
+
+    # df[df.name != 'Tina']
+    df=data=data[data.filename!=ran_file[loop]]
+    count_val=data.groupby('class').count()
+    # print(count_val.iloc[2, 1])
+    if count_val.iloc[2,1] < 35000:
+        print(data.groupby('class').count())
+        data.to_csv("Berkerly_final_Dataseets.csv")
+        break
+    loop=loop+1
+
+# pyindex=np.random.choice(index, size=30000, replace=False)
+# data.drop(data.index[pyindex],inplace=True)
+# print(data.groupby('class').count())'''
--- Python/python_code/prac.py
+++ Python/python_code/prac.py
@@ -1,22 +1,84 @@
 import pandas as pd
 import numpy as np
-data=pd.read_csv("/home/mayank-s/Desktop/berkely_filtered.csv")
+
+'''data=pd.read_csv('Carcount.csv')
+data=data.sort_values(by='count', ascending=False)
+print(data)
+
+df=data[data['count']>12]
+print(df)
+# df.to_csv('maxcarcaount_3.csv')
+# df['xmax']-df['xmin'] > threshold
+# df[df['JobTitle'].value_counts()<2])'''
+
+
+
+
+data=pd.read_csv("/home/mayank-s/PycharmProjects/Datasets/Berkely_DeepDrive/berkely_train.csv")
 # data=pd.read_csv('/home/mayank-s/Desktop/Link to Datasets/aptiveBB/reddy.csv')
 print(data.head())
 print(data.groupby('class').count())
 
-# datasets[(datasets['Pclass']>1) & (datasets['Age']>25)].head()
+# this is done to remove if xmin==xmax and ymin==yamax(which is actuallly wrong)
 df=data[(data['xmin']!=data['xmax']) & (data['ymin']!=data['ymax'])]
 
 
 print(df.head())
 
-df.to_csv("berkely_After_filter.csv")
-'''mydata=data.groupby('class')
-print(data.groupby('class').count())
-index=mydata.groups['car'].values
+# df.to_csv("berkely_After_filter.csv")
 
-pyindex=np.random.choice(index, size=10000, replace=False)
+# this is most important funtion to count no of class in group
+new=data.groupby(['filename'])['class'].count()
+
+
+gb = data.groupby(['filename'])
+grouped_C = gb['class']
+n=data.groupby(['filename', 'class'])
+a=(n.size())
+print(a)
+gv=a.index[0]
+
+for file_name, (cls) in enumerate(a):
+    print(file_name)
+    print(cls)
+new1=data.groupby(['filename', 'class'])['xmin']#.count()
+
+# b=data.groupby(level=['filname', 'class']).sum()
+
+
+
+
+mydata=data.groupby('filename')
+print(data.groupby('class').count())
+len_group=mydata.ngroups
+# index=mydata.groups['car'].values
+mygroup=mydata.groups
+
+# new=data.groupby(['filename', 'class'])#['car'].count()
+
+# this is most important funtion to count no of class in group
+new=data.groupby(['filename'])['class'].count()
+
+for da in mygroup.keys():
+    index = mydata.groups[da].values
+    for read_index in index():
+
+
+        print(index)
+        print(da)
+        break
+
+
+
+'''for da in mydata.ngroups():
+
+    index = mydata.groups['car'].values
+    mydata.groups['0124dfa6-385f1b58'].values
+    print(da)'''
+
+
+# index=mydata.groups['car'].values
+'''pyindex=np.random.choice(index, size=10000, replace=False)
 data.drop(data.index[pyindex],inplace=True)
 print(data.groupby('class').count())
 
